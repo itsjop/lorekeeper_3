3fd6caf2b6fe46a1ed0666889145cce7f21f9ed1
DESKTOP-01VSOB5\Safira Island <animatedcritter@gmail.com>
2023-03-03 22:32:23 -0600

    Feat: finish mod-mail admin views

--- resources/views/admin/mail/mail.blade.php
+++ resources/views/admin/mail/mail.blade.php
@@ -9,14 +9,31 @@
 
 <div class="card">
     <div class="card-header">
-        <h3 class="card-title">Subject: {{$mod->subject}}</h3>
+        <div class="row">
+            <div class="col-6">
+                <h3>Subject: {{ $mail->subject }}</h3>
+            </div>
+            <div class="col-6 text-right">
+               <h5>Sent {!! pretty_date($mail->created_at) !!}</h5>
+            </div>
+        </div>
     </div>
     <div class="card-body">
         @if(Auth::check() && Auth::user()->isStaff)
-            <p>From: {{$mod->staff->displayName}} {{ add_help('Not viewable by users.') }}</p>
+            <p class="float-right ml-5">From: {!! $mail->staff->displayName !!} {!! add_help('Not viewable by users.') !!}</p>
         @endif
-        <p>To: {{$mod->user->displayName}}</p>
-        <p>{{$mod->message}}</p>
+        <p>To: {!! $mail->user->displayName !!} {!! $mail->seen ? '<i class="fas fa-eye text-success"></i>' : '<i class="fas fa-eye-slash text-danger"></i>' !!}</p>
+        <hr>
+        <p>{!! $mail->message !!}</p>
+        <hr>
+        <div class="card-text {!! $mail->issue_strike ? 'text-danger' : 'text-secondary' !!} float-right">
+            @if(!$mail->issue_strike)
+                <h5>No Strike Issued</h5>
+            @else
+                <h5>Strike{{ $mail->strike_count > 1 ? 's' : ''}} Issued</h5>
+                <strong>Amount:</strong> {{ $mail->strike_count }}
+            @endif
+        </div>
     </div>
 </div>
 @endsection
