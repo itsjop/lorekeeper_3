e96928be2d5f63bf4fcc5456990b97a656495072
DESKTOP-01VSOB5\Safira Island <animatedcritter@gmail.com>
2023-03-27 02:27:28 -0500

    Feat: add user-pronouns to extension tracker

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -13,4 +13,12 @@ return [
     // ],
     
 
+    'user_pronouns' => [
+        'key' => 'user_pronouns',
+        'wiki_key' => 'User_Pronouns',
+        'creators' => json_encode([
+            'AnimatedCritter' => 'https://github.com/animatedcritter/',
+        ]),
+        'version' => '1.0.0',
+    ],
 ];
