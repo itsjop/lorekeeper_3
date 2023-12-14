90ee47a924d14d74c05b147b9a5946aba3fc433b
Uri <45401276+preimpression@users.noreply.github.com>
2023-12-14 11:44:18 -0500

    Update extension_tracker.php

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -19,7 +19,7 @@ return [
             'Uri' => 'https://github.com/preimpression/',
             'Mercury' => 'https://github.com/itinerare/',
         ]),
-        'version' => '1.3.1',
+        'version' => '1.3.2',
     ],
 
 
