785389363659780a2137df74059a3d644ca19984
ScuffedNewt <dragonminer101@gmail.com>
2024-10-25 00:56:14 +0100

    chore: run lint

---
+++ config/lorekeeper/mod_mail.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    // Can users respond to staff mail?
+    'can_user_respond_to_staff_mail' => false,
+
+];
--- resources/views/comments/comments.blade.php
+++ resources/views/comments/comments.blade.php
@@ -68,39 +68,39 @@
 @endisset
 
 @auth
-    @include('comments._form')
+        @include('comments._form')
 @else
     <div class="card mt-3">
-        <div class="card-body">
-            <h5 class="card-title">Authentication required</h5>
-            <p class="card-text">You must log in to post a comment.</p>
-            <a href="{{ route('login') }}" class="btn btn-primary">Log in</a>
+            <div class="card-body">
+                <h5 class="card-title">Authentication required</h5>
+                <p class="card-text">You must log in to post a comment.</p>
+                <a href="{{ route('login') }}" class="btn btn-primary">Log in</a>
+            </div>
         </div>
-    </div>
 @endauth
 
 @section('scripts')
     @parent
-    <script>
-        $(document).ready(function() {
-            tinymce.init({
-                selector: '.comment-wysiwyg',
-                height: 250,
-                menubar: false,
-                convert_urls: false,
-                plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen spoiler',
-                    'insertdatetime media table paste code help wordcount'
-                ],
-                toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | code',
-                content_css: [
-                    '{{ asset('css/app.css') }}',
-                    '{{ asset('css/lorekeeper.css') }}'
-                ],
-                spoiler_caption: 'Toggle Spoiler',
-                target_list: false
+        <script>
+            $(document).ready(function() {
+                tinymce.init({
+                    selector: '.comment-wysiwyg',
+                    height: 250,
+                    menubar: false,
+                    convert_urls: false,
+                    plugins: [
+                        'advlist autolink lists link image charmap print preview anchor',
+                        'searchreplace visualblocks code fullscreen spoiler',
+                        'insertdatetime media table paste code help wordcount'
+                    ],
+                    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | code',
+                    content_css: [
+                        '{{ asset('css/app.css') }}',
+                        '{{ asset('css/lorekeeper.css') }}'
+                    ],
+                    spoiler_caption: 'Toggle Spoiler',
+                    target_list: false
+                });
             });
-        });
-    </script>
+        </script>
 @endsection
