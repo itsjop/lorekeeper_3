fc64742ba5aee10db8042723566539e2628c135b
Jordan Crandall <me@jop.is>
2025-06-22 16:35:30 -0400

    Sidebar and site fixes

--- resources/views/pages/_sidebar.blade.php
+++ resources/views/pages/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="featured-character">
   <div class="sidebar-header">
-    <a href="#" class="card-link">Featured Character</a>
+    <a href="#" class="card-link">Star of the Week!</a>
   </div>
 
   <div class="sidebar-section p-2">
@@ -9,17 +9,27 @@
         <div class="character-border">
           <div class="rainbow"></div>
           <div class="stars"></div>
+          <div class="char-filter"></div>
         </div>
         <img
-            src="{{ $featured->image->canViewFull(Auth::user() ?? null) && file_exists(public_path($featured->image->imageDirectory . '/' . $featured->image->fullsizeFileName)) ? $featured->image->fullsizeUrl : $featured->image->imageUrl }}"
-         class="img-char-thumbnail" />
+          src="{{ $featured->image->canViewFull(Auth::user() ?? null) && file_exists(public_path($featured->image->imageDirectory . '/' . $featured->image->fullsizeFileName)) ? $featured->image->fullsizeUrl : $featured->image->imageUrl }}"
+          class="img-char-thumbnail"
+        />
       </a>
-      <a class="name" href="{{ $featured->url }}" class="h5 mb-0">
+      <a
+        class="name"
+        href="{{ $featured->url }}"
+        class="h5 mb-0"
+      >
         @if (!$featured->is_visible)
           <i class="fas fa-eye-slash"></i>
         @endif {{ $featured->fullName }}
       </a>
-      <a class="owner" href="{{ $featured->url }}" class="h5 flex mb-0">
+      <a
+        class="owner"
+        href="{{ $featured->url }}"
+        class="h5 flex mb-0"
+      >
         {!! $featured->displayOwner !!}
       </a>
       <div class="meta" class="small">
