870240dff3b72bf4366fa83e74c44caa0ff67434
Jordan Crandall <me@jop.is>
2025-07-06 19:48:04 -0400

    Revert "chore: update ext tracker info"
    This reverts commit 80cfaff0e2e2ee810c7fd3accff33df00dd52306.

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -164,13 +164,4 @@ return [
     ]),
     'version' => '2.0.0',
   ],
-  'selected_character' => [
-    'key' => 'selected_character',
-    'wiki_key' => 'Selected_Character',
-    'creators' => json_encode([
-      'Mercury' => 'https://github.com/itinerare/',
-    ]),
-    'version' => '1.0.1',
-  ],
-
 ];
