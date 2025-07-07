5ba9fe5ce7938e10f033585657db1769443e201c
Jordan Crandall <me@jop.is>
2025-07-07 01:48:20 -0400

    too many commits

--- resources/views/world/_features_index.blade.php
+++ resources/views/world/_features_index.blade.php
@@ -1,11 +1,17 @@
 @foreach ($features as $categoryId => $categoryFeatures)
-  @if (!isset($categories[$categoryId]) || (Auth::check() && Auth::user()->hasPower('edit_data')) || $categories[$categoryId]->is_visible)
+  @if (
+      !isset($categories[$categoryId]) ||
+          (Auth::check() && Auth::user()->hasPower('edit_data')) ||
+          $categories[$categoryId]->is_visible
+  )
     <div class="card mb-3 inventory-category">
       <h5 class="card-header inventory-header">
         @if (isset($categories[$categoryId]) && !$categories[$categoryId]->is_visible)
           <i class="fas fa-eye-slash mr-1"></i>
         @endif
-        {!! isset($categories[$categoryId]) ? '<a href="' . $categories[$categoryId]->searchUrl . '">' . $categories[$categoryId]->name . '</a>' : 'Miscellaneous' !!}
+        {!! isset($categories[$categoryId])
+            ? '<a href="' . $categories[$categoryId]->searchUrl . '">' . $categories[$categoryId]->name . '</a>'
+            : 'Miscellaneous' !!}
       </h5>
       <div class="card-body inventory-body">
         <div class="grid grid-4-col gap-2">
@@ -16,12 +22,16 @@
                 <div class="stars"></div>
               </div>
               @if ($feature->first()->has_image)
-                <a class="badge" style="border-radius:.5em; ">
-                  <img class="my-1 modal-image" src="{{ $feature->first()->imageUrl }}" alt="{{ $feature->first()->name }}" data-id="{{ $feature->first()->id }}" />
+                <a class="badge">
+                  <img
+                    class="feature-image"
+                    src="{{ $feature->first()->imageUrl }}"
+                    alt="{{ $feature->first()->name }}"
+                    data-id="{{ $feature->first()->id }}"
+                  />
                 </a>
               @else
-                <a class="badge" style="border-radius:.5em; ">
-                </a>
+                <a class="badge"> </a>
               @endif
 
               @if (!$feature->first()->is_visible)
