669bf001334c081a35bfe24f5cf964f44ef7ad07
itsjop <itsjop@users.noreply.github.com>
2025-07-12 02:35:06 +0000

    refactor: fix blade formatting

--- resources/views/browse/_masterlist_content_entry.blade.php
+++ resources/views/browse/_masterlist_content_entry.blade.php
@@ -7,15 +7,11 @@
         {{ ucfirst(getSubtypeInfo($character->image->subtype_id)) }} Palate
       </div>
     </div>
-    <img src="{{ asset('images/subtypes/badges/' . getSubtypeInfo($character->image->subtype_id) . '.png') }}"
-      alt="{{ 'Subtype badge for ' . $character->url . '.' }}"
-    >
+    <img src="{{ asset('images/subtypes/badges/' . getSubtypeInfo($character->image->subtype_id) . '.png') }}" alt="{{ 'Subtype badge for ' . $character->url . '.' }}">
   </div>
   <div class="border-background"></div>
   {{-- Name --}}
-  <div
-    class="name {{ strlen($character->nameFallback) > 14 ? 'name-sm' : (strlen($character->nameFallback) < 8 ? 'name-lg' : '') }}"
-  >
+  <div class="name {{ strlen($character->nameFallback) > 14 ? 'name-sm' : (strlen($character->nameFallback) < 8 ? 'name-lg' : '') }}">
     <a href="{{ $character->url }}" class="">
       @if (!$character->is_visible)
         <i class="fas fa-eye-slash"></i>
@@ -31,9 +27,7 @@
       <img
         src="
       {{ $character->image->canViewFull(Auth::user() ?? null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)) ? $character->image->thumbnailUrl : $character->image->thumbnailUrl }}"
-        class="ml-thumbnail"
-        alt="Thumbnail for {{ $character->nameFallback }}"
-      />
+        class="ml-thumbnail" alt="Thumbnail for {{ $character->nameFallback }}" />
     </div>
   </a>
   {{-- Character Number ID --}}
--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,13 +1,5 @@
 <nav class="navbar navbar-expand-md" id="header-nav">
-  <button
-    class="navbar-toggler collapsed"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent"
-    aria-expanded="false"
-    aria-label="{{ __('Toggle navigation') }}"
-  >
+  <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
     <span class="navbar-toggler-icon">
       <span class="line"></span>
       <span class="line"></span>
@@ -26,16 +18,7 @@
     <!-- Left Side Of Navbar -->
     <ul id="site-navbar" class=" navbar-nav">
       <li class="nav-item dropdown">
-        <div
-          id="inventoryDropdown"
-          class="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          v-pre
-        >
+        <div id="inventoryDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
           Site
         </div>
         <div class="dropdown-menu dt-nav-page" aria-labelledby="inventoryDropdown">
@@ -51,16 +34,7 @@
       </li>
       @if (Auth::check())
         <li class="nav-item dropdown">
-          <div
-            id="inventoryDropdown"
-            class="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-            v-pre
-          >
+          <div id="inventoryDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             Account
           </div>
           <div class="dropdown-menu dt-nav-page" aria-labelledby="inventoryDropdown">
@@ -96,16 +70,7 @@
         </li>
       @endif
       <li class="nav-item dropdown">
-        <div
-          id="queueDropdown"
-          class="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          v-pre
-        >
+        <div id="queueDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
           Info
         </div>
         <div class="dropdown-menu dt-nav-page" aria-labelledby="inventoryDropdown">
@@ -130,16 +95,7 @@
         </div>
       </li>
       <li class="nav-item dropdown">
-        <div
-          id="browseDropdown"
-          class="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          v-pre
-        >
+        <div id="browseDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
           Guides
         </div>
         <div class="dropdown-menu dt-nav-page" aria-labelledby="inventoryDropdown">
@@ -173,16 +129,7 @@
         </div>
       </li>
       <li class="nav-item dropdown">
-        <div
-          id="loreDropdown"
-          class="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          v-pre
-        >
+        <div id="loreDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
           Masterlist
         </div>
         <div class="dropdown-menu dt-nav-page" aria-labelledby="inventoryDropdown">
@@ -200,16 +147,7 @@
         </div>
       </li>
       <li class="nav-item dropdown">
-        <div
-          id="playDropdown"
-          class="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          v-pre
-        >
+        <div id="playDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
           Play
         </div>
         <div class="dropdown-menu dt-nav-page" aria-labelledby="inventoryDropdown">
@@ -235,16 +173,7 @@
         </div>
       </li>
       <li class="nav-item dropdown">
-        <div
-          id="communityDropdown"
-          class="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          v-pre
-        >
+        <div id="communityDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
           Community
         </div>
         <div class="dropdown-menu dt-nav-page" aria-labelledby="inventoryDropdown">
@@ -293,16 +222,7 @@
           </li>
         @endif
         <li class="nav-item dropdown">
-          <div
-            id="browseDropdown"
-            class="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-            v-pre
-          >
+          <div id="browseDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             Submit
           </div>
           <div class="dropdown-menu dropdown-menu-right dt-nav-page" aria-labelledby="browseDropdown">
@@ -317,16 +237,7 @@
           </div>
         </li>
         <li class="nav-item dropdown">
-          <div
-            id="navbarDropdown"
-            class="nav-link dropdown-toggle"
-            href="{{ Auth::user()->url }}"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-            v-pre
-          >
+          <div id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ Auth::user()->url }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             {{ Auth::user()->name }} <span class="caret"></span>
           </div>
           <div class="dropdown-menu dropdown-menu-right dt-nav-page" aria-labelledby="navbarDropdown">
@@ -339,19 +250,10 @@
                 <i class="fas fa-bookmark"></i> Bookmarks </a>
               <a class="dt-nav-item" href="{{ url('account/settings') }}">
                 <i class="fas fa-cog"></i> Settings </a>
-              <a
-                class="dt-nav-item"
-                href="{{ route('logout') }}"
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-              >
+              <a class="dt-nav-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }} </a>
               </a>
-              <form
-                id="logout-form"
-                action="{{ route('logout') }}"
-                method="POST"
-                style="display: none;"
-              >
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
               </form>
             </div>
