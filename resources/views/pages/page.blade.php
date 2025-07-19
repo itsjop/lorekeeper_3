6b0740c040b2f4743a2c4718a440bd56ccddc919
Jordan Crandall <me@jop.is>
2025-07-19 17:52:57 -0400

    Lore page update

--- resources/views/pages/page.blade.php
+++ resources/views/pages/page.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app', ['pageName' => '/pages'])
 
-
 @section('title')
   {{ $page->title }}
 @endsection
 
 @section('content')
- <x-admin-edit title="Page" :object="$page" />
-  {{-- {!! breadcrumbs([$page->title => $page->url]) !!}
+  <div id="custom-page">
+    <x-admin-edit title="Page" :object="$page" />
+    {{-- {!! breadcrumbs([$page->title => $page->url]) !!}
   <h1>
     @if (!$page->is_visible)
-      <i class="fas fa-eye-slash mr-1"></i>
+    <i class="fas fa-eye-slash mr-1"></i>
     @endif
     {{ $page->title }}
   </h1>
@@ -19,17 +19,18 @@
     <div> Last updated {!! format_date($page->updated_at) !!}</div>
   </div> --}}
 
-  <div class="site-page-content parsed-text">
-    {!! $page->parsed_text !!}
-  </div>
-
-  @if ($page->can_comment)
-    <div class="container">
-      @comments([
-          'model' => $page,
-          'perPage' => 5,
-          'allow_dislikes' => $page->allow_dislikes,
-      ])
+    <div class="site-page-content parsed-text">
+      {!! $page->parsed_text !!}
     </div>
-  @endif
+
+    @if ($page->can_comment)
+      <div class="container">
+        @comments([
+            'model' => $page,
+            'perPage' => 5,
+            'allow_dislikes' => $page->allow_dislikes
+        ])
+      </div>
+    @endif
+  </div>
 @endsection
