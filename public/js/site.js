6ae7a18c52dae6e2d55a9a1f3e8eda65bfefc04c
Jordan Crandall <me@jop.is>
2025-07-20 22:07:04 -0400

    Console.log removal

--- public/js/site.js
+++ public/js/site.js
@@ -2,7 +2,7 @@ function loadModal(url, title) {
   $('#modal').find('.modal-body').html('');
   $('#modal').find('.modal-title').html(title);
   $('#modal').find('.modal-body').load(url, ( response, status, xhr ) => {
-    console.log("response", response, url, title);
+    // console.log("response", response, url, title);
     if ( status === "error" ) {;
       // console.log("error", xhr);
       $( "#modal" ).find('.modal-body').html( response );
