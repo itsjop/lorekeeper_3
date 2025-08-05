8be7b8aaa314c2b241da63669665d4d63decf40b
Jordan Crandall <me@jop.is>
2025-08-05 16:04:14 -0400

    Wyspissues

--- resources/views/character/_pet_bonding_info.blade.php
+++ resources/views/character/_pet_bonding_info.blade.php
@@ -1,22 +1,31 @@
 <div class="pet-card card ji-center ai-center gap-_5">
-  <img src="{{ $pet->pet->image($pet->id) }}" class="rounded img-fluid" style="max-width: 150px;"/>
+  <img
+    src="{{ $pet->pet->image($pet->id) }}"
+    class="rounded img-fluid"
+    style="max-width: 150px;"
+  />
+
   @if ($pet->pet_name)
-    <div class="title grid br-15 jc-center ji-center " style="color">
-      <div class="petname w-min br-15 py-1 px-2 h4" style="color: white;">
+    <div class="title grid br-15 jc-center ji-center ">
+      <div class="petname w-min br-15 py-1 px-2 h4" style="color: white; background-color:rgba(119, 76, 198, 0.599);">
         {!! $pet->pet_name !!}
       </div>
     </div>
-    @else
+  @else
     <div class="title grid br-15 jc-center ji-center ">
       <div class=" small text-secondary w-min br-15 py-1 px-2" style="font-size:95%;">
         ---
       </div>
     </div>
-    @endif
-  <div class="name flex gap-_5 h-100 jc-center ai-end text-center">
+  @endif
+
+  <div
+    class="name flex gap-_5 h-100 jc-center ai-end text-center h3"style="font-style: italic; color:rgb(103, 77, 156); font-size:1rem;"
+  >
     {!! $pet->pet->displayName !!}
   </div>
-  <div class="progress as-end h-90 br-15 w-80">
+
+  <div class="progress as-end h-90 br-15 w-60">
     <div
       class="progress-bar progress-bar-striped progress-bar-animated"
       role="progressbar"
@@ -28,7 +37,7 @@
       {{ $pet->level?->nextLevel?->bonding_required ? $pet->level?->bonding . '/' . $pet->level?->nextLevel?->bonding_required : 'Max' }}
     </div>
   </div>
-  <div class="level" style="font-style: italic;">
+  <div class="level h3" style="font-style: italic; color:rgb(103, 77, 156); font-size:1rem;">
     {{ $pet->level?->levelName }}
   </div>
 </div>
