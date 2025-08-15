5d6b3588ea1aa1022e26ec2ad6252b230d9b8304
Jordan Crandall <me@jop.is>
2025-08-15 02:21:34 -0400

    Logged out Dashboard

--- resources/views/pages/_logged_out.blade.php
+++ resources/views/pages/_logged_out.blade.php
@@ -1,153 +1,166 @@
-<div class="card-basic">
-  <p style="font-size: 1.2rem;"><strong> Somnivores.com is in early access! <br /></strong>Registration is currently closed, please
-    stay tuned for our full launch. Follow our <strong><a
-        href="https://toyhou.se/~forums/5868.species-discussion/563827.somnivores-18-cs-lk-beta-closed"
-      >Toyhouse Thread</a></strong> for updates!</p>
-</div>
-<h1 class="header"><img
-    class="w-content-100"
-    src="/images/lore_pages/homepage/splashart.png"
-    alt="Somnivores splash art"
-  /></h1>
-<div class="card-basic">
-  <h1 class="top">What is a Somnivore?</h1>
-  <hr />
-  <div class="card-text">
-    <h2>Somnivores are small magical creatures that eat dreams!</h2>
-    <p>Their primary form is that of a small bat-like creature; their most distinct features being their dazzling horns and wings
-      which are made of the dream essence they consume. Other notable features include their bat ears, fangs, and thin, long tails
-      that end in a tuft of fur.</p>
+<div class="lore">
+  <div class="card-basic">
+    <p style="font-size: 1.2rem;"><strong> Somnivores.com is in early access! <br /></strong>Registration is currently closed,
+      please
+      stay tuned for our full launch. Follow our <strong><a
+          href="https://toyhou.se/~forums/5868.species-discussion/563827.somnivores-18-cs-lk-beta-closed"
+        >Toyhouse Thread</a></strong> for updates!</p>
   </div>
-  <div class="card-img"><img
-      src="/images/lore_pages/homepage/keytraits.png"
-      alt="Somnivores Key Traits"
-      width="606"
-      height="444"
-    /></div>
-  <div class="card-text">
-    <p>Similar to their vampire ancestors, somnivores possess the ability to shapeshift from their bat form into a humanoid - this
-      is referred to as their demon form. <br />These demons often sport unnatural skin tones and always maintain their horns and
-      spindly tail, along with many other optional features that may be translated from their bat&rsquo;s appearance!</p>
+  <h1 class="header"><img
+      class="w-content-100"
+      src="/images/lore_pages/homepage/splashart.png"
+      alt="Somnivores splash art"
+    /></h1>
+  <div class="card-basic">
+    <h1 class="top">What is a Somnivore?</h1>
+    <hr />
+    <div class="card-text">
+      <h2>Somnivores are small magical creatures that eat dreams!</h2>
+      <p>Their primary form is that of a small bat-like creature; their most distinct features being their dazzling horns and
+        wings
+        which are made of the dream essence they consume. Other notable features include their bat ears, fangs, and thin, long
+        tails
+        that end in a tuft of fur.</p>
+    </div>
+    <div class="card-img"><img
+        src="/images/lore_pages/homepage/keytraits.png"
+        alt="Somnivores Key Traits"
+        width="606"
+        height="444"
+      /></div>
+    <div class="card-text">
+      <p>Similar to their vampire ancestors, somnivores possess the ability to shapeshift from their bat form into a humanoid -
+        this
+        is referred to as their demon form. <br />These demons often sport unnatural skin tones and always maintain their horns
+        and
+        spindly tail, along with many other optional features that may be translated from their bat&rsquo;s appearance!</p>
+    </div>
+    <div class="card-img"><img
+        class="w-100"
+        src="/images/lore_pages/homepage/demonforms.png"
+        alt="Demonstration of the Demon Form"
+      /></div>
   </div>
-  <div class="card-img"><img
-      class="w-100"
-      src="/images/lore_pages/homepage/demonforms.png"
-      alt="Demonstration of the Demon Form"
-    /></div>
-</div>
-<div class="card-basic">
-  <h1 class="top">Dream Diet</h1>
-  <hr />
-  <div class="card-text">
-    <p>Somnivores sustain themselves &amp; their magical abilities by consuming dream essence. This astral, fog-like substance
-      forms around a dreamer&rsquo;s head as they sleep. Dream essence consists of stars, sparkles, and clouds- all swirling
-      slowly around each other and producing a soft glow.</p>
+  <div class="card-basic">
+    <h1 class="top">Dream Diet</h1>
+    <hr />
+    <div class="card-text">
+      <p>Somnivores sustain themselves &amp; their magical abilities by consuming dream essence. This astral, fog-like substance
+        forms around a dreamer&rsquo;s head as they sleep. Dream essence consists of stars, sparkles, and clouds- all swirling
+        slowly around each other and producing a soft glow.</p>
+    </div>
+    <div class="card-img"><img
+        src="/images/lore_pages/homepage/dreaming.png"
+        alt="Demonstration of the Dream Essence"
+        width="455"
+        height="488"
+      /></div>
+    <div class="card-text">Every dream is unique and the resulting dream essence possesses an appearance and flavor influenced by
+      the contents and feelings within.Somnivores are very picky eaters, each one having their personal palate limited to only one
+      or two of the six core classifications of dream. <br />You can always tell which types of dreams a somnivore favors by
+      looking
+      at the colors displayed by their softly glowing horns and wings!</div>
   </div>
-  <div class="card-img"><img
-      src="/images/lore_pages/homepage/dreaming.png"
-      alt="Demonstration of the Dream Essence"
-      width="455"
-      height="488"
-    /></div>
-  <div class="card-text">Every dream is unique and the resulting dream essence possesses an appearance and flavor influenced by
-    the contents and feelings within.Somnivores are very picky eaters, each one having their personal palate limited to only one
-    or two of the six core classifications of dream. <br />You can always tell which types of dreams a somnivore favors by looking
-    at the colors displayed by their softly glowing horns and wings!</div>
-</div>
-<div class="card-basic">
-  <h1 class="top">Reverie</h1>
-  <hr />
-  <div class="card-img"><img
-      class="w-100 br-15"
-      src="/images/lore_pages/homepage/reveriesmall.png"
-      alt="World Map of Reverie"
-    /></div>
-  <div class="card-text"><br />
-    <p>Reverie is the magical realm in which the vast majority of the somnivore population resides. Founded by the first colony of
-      somnivores, this mysterious world is a sanctuary for magical beings where the night is eternal and the air hums with mana.
-      <br />A lot of things we're familiar with in our everyday lives have a counterpart that exists in Reverie- though through a
-      bit of a "dream-like" lens. Plants glow, clouds can be built on, the breeze carries stardust. Buildings and technology can
-      almost appear cartoonish with their soft shapes and glowing parts. There&rsquo;s no shortage of mystical places to visit and
-      interesting things to do! <br />In a thriving age of abundant dreams, somnivores value community, leisure, and curiosity for
-      the expanding whimsical world they reside in.</p>
+  <div class="card-basic">
+    <h1 class="top">Reverie</h1>
+    <hr />
+    <div class="card-img"><img
+        class="w-100 br-15"
+        src="/images/lore_pages/homepage/reveriesmall.png"
+        alt="World Map of Reverie"
+      /></div>
+    <div class="card-text"><br />
+      <p>Reverie is the magical realm in which the vast majority of the somnivore population resides. Founded by the first colony
+        of
+        somnivores, this mysterious world is a sanctuary for magical beings where the night is eternal and the air hums with mana.
+        <br />A lot of things we're familiar with in our everyday lives have a counterpart that exists in Reverie- though through
+        a
+        bit of a "dream-like" lens. Plants glow, clouds can be built on, the breeze carries stardust. Buildings and technology can
+        almost appear cartoonish with their soft shapes and glowing parts. There&rsquo;s no shortage of mystical places to visit
+        and
+        interesting things to do! <br />In a thriving age of abundant dreams, somnivores value community, leisure, and curiosity
+        for
+        the expanding whimsical world they reside in.
+      </p>
+    </div>
   </div>
-</div>
-<h1 class="header" style="margin-bottom: 0;">⟡˙⋆ Welcome to the Somnivores ARPG! ⋆˙⟡</h1>
-<h2>What we offer:</h2>
-<div class="card-basic grid grid-3-col w-content-66">
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/characters.png"
-      alt=""
-    />
-    <p>A unique subtype system and 30+ traits to mix and match, with more to come 🦇</p>
+  <h1 class="header" style="margin-bottom: 0;">⟡˙⋆ Welcome to the Somnivores ARPG! ⋆˙⟡</h1>
+  <h2>What we offer:</h2>
+  <div class="card-basic grid grid-3-col w-content-66">
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/characters.png"
+        alt=""
+      />
+      <p>A unique subtype system and 30+ traits to mix and match, with more to come 🦇</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/starlitacres.png"
+        alt=""
+      />
+      <p>Dailies that passively reward species currency 💰</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/prompts.png"
+        alt=""
+      />
+      <p>Permanent and repeatable prompts that award species currency and titles! 🎁</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/shops.png"
+        alt=""
+      />
+      <p>Monthly events with limited prompts and seasonal traits and items 🎉</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/petshopicon.png"
+        alt=""
+      />
+      <p>Curious critters to collect! There are currently 15 unique pets to collect; 40 if you include their variants and
+        evolutions.</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/inventory.png"
+        alt=""
+      />
+      <p>Trinkets to put in your character&rsquo;s inventories, which will be displayed directly on their ML profile 🎀</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/worldmap.png"
+        alt=""
+      />
+      <p>A developed world with fleshed out locations and lore! 🌎</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/account.png"
+        alt=""
+      />
+      <p>Whimsical and playful aesthetics inspired by dreams, magic, and a bit of nostalgia ✨</p>
+    </div>
+    <div><img
+        class="w-100"
+        src="/images/lore_pages/homepage/supporter.png"
+        alt=""
+      />
+      <p>A welcoming community that inspires creativity! :D</p>
+    </div>
   </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/starlitacres.png"
-      alt=""
-    />
-    <p>Dailies that passively reward species currency 💰</p>
+  <div class="card-basic">
+    <h1 class="top">What&rsquo;s to come!</h1>
+    <hr />
+    <ul>
+      <li>More dailies! Namely foraging 🌿</li>
+      <li>Recipes &amp; Crafting 🔨</li>
+      <li>A new shop for spending dream essence, along with new ways to earn more! ✨</li>
+      <li>More trinkets! More Pets! 🧸</li>
+      <li>&amp; Much more, follow along to see what&rsquo;s in store! 🌠</li>
+    </ul>
   </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/prompts.png"
-      alt=""
-    />
-    <p>Permanent and repeatable prompts that award species currency and titles! 🎁</p>
-  </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/shops.png"
-      alt=""
-    />
-    <p>Monthly events with limited prompts and seasonal traits and items 🎉</p>
-  </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/petshopicon.png"
-      alt=""
-    />
-    <p>Curious critters to collect! There are currently 15 unique pets to collect; 40 if you include their variants and
-      evolutions.</p>
-  </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/inventory.png"
-      alt=""
-    />
-    <p>Trinkets to put in your character&rsquo;s inventories, which will be displayed directly on their ML profile 🎀</p>
-  </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/worldmap.png"
-      alt=""
-    />
-    <p>A developed world with fleshed out locations and lore! 🌎</p>
-  </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/account.png"
-      alt=""
-    />
-    <p>Whimsical and playful aesthetics inspired by dreams, magic, and a bit of nostalgia ✨</p>
-  </div>
-  <div><img
-      class="w-100"
-      src="/images/lore_pages/homepage/supporter.png"
-      alt=""
-    />
-    <p>A welcoming community that inspires creativity! :D</p>
-  </div>
-</div>
-<div class="card-basic">
-  <h1 class="top">What&rsquo;s to come!</h1>
-  <hr />
-  <ul>
-    <li>More dailies! Namely foraging 🌿</li>
-    <li>Recipes &amp; Crafting 🔨</li>
-    <li>A new shop for spending dream essence, along with new ways to earn more! ✨</li>
-    <li>More trinkets! More Pets! 🧸</li>
-    <li>&amp; Much more, follow along to see what&rsquo;s in store! 🌠</li>
-  </ul>
 </div>
