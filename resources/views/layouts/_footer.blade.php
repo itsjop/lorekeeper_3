f9ebccf025570a2c045c3c1a0697f74474607a9d
Jordan Crandall <me@jop.is>
2025-08-21 18:34:28 -0400

    footer reverted

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -16,39 +16,10 @@
       <a href="{{ url('credits') }}" class="nav-link">Credits</a>
     </li>
   </ul>
-  {{-- <div class="copyright">
-    &copy; {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}
+  <div class="copyright">z&copy; {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}
     v{{ config('lorekeeper.settings.version') }} {{ Carbon\Carbon::now()->year }}
-  </div> --}}
-  <div
-    id="debug"
-    class="copyright"
-    style="opacity: 0.4; font-size: .5rem;"
-  >
-    <pre id="" class="m-0">
-      temp debugging info pls ignore:
-    </pre>
-    <pre id="wid" class="m-0"></pre>
   </div>
   @if (config('lorekeeper.extensions.scroll_to_top'))
     @include('widgets/_scroll_to_top')
   @endif
 </footer>
-<script>
-  $(document).ready(function() {
-    $('#wid').text(`
-      availWidth: ${window.screen.availWidth}, ratio: ${window.devicePixelRatio}
-    `);
-  })
-</script>
-<style>
-  #debug {
-    text-align: end;
-  }
-
-  @container main-container (width > 600px) {
-    #debug {
-      display: none;
-    }
-  }
-</style>
