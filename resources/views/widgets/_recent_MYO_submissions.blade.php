054baf595c369e900d6348305f055c522d006b6c
Jordan Crandall <me@jop.is>
2025-08-28 22:21:33 -0400

    Mainpage rollback

--- resources/views/widgets/_recent_MYO_submissions.blade.php
+++ resources/views/widgets/_recent_MYO_submissions.blade.php
@@ -1,4 +1,4 @@
-<div class="gallery-recents grid-4-col singlerow gap-4 ji-between text-center w-100">
+<div class="gallery-recents grid-4-col singlerow ji-between text-center w-100">
   @foreach ($myos as $character)
     @include('browse._masterlist_content_entry', [
         'char_image' =>
